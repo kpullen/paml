d9dad22d6c433cceae1a1c691195b0737f87bd7e
Kenneth Pullen <kenneth.pullen@gmail.com>
2013-11-06 23:25:51 -0800

    Clean up Node specs
    The Node specs were out of control, so I nested a bunch of related tests
    in contexts, and removed some tests that tested things link the intro
    and outro as the Node will soon not have readers for those variables.

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,26 +2,28 @@ require "spec_helper"
 include Paml
 
 describe Node do
-	let(:single_mom) { parent = Node.new; parent << Node.new; parent }
-	let(:has_attributes) { Node.from_hash(attributes: "a: 1, b: 2") }
+	context "when constructing nodes" do
+		context "when accepting pre-element whitespace" do
+			it("accepts tabs") { expect(Node.from_hash(whitespace: "\t").level).to eq 1 }
+			it("doesn't accept spaces") { expect(Node.from_hash(whitespace: " ").level).to eq 0 }
 
-	it("has a level") { Node.from_hash(whitespace: "\t").level.should eq 1 }
-	it("only accepts tabs as whitespace") { Node.from_hash(whitespace: " ").level.should eq 0 }
+			subject { Node.from_hash attributes: "a: 1, b: 2" }
+			it("parses an attribute string") { expect(subject.to_s).to eq "<div a='1' b='2'></div>" }
+		end
 
-	it("is a div by default") { Node.new.intro.should eq "<div>" }
+		context "when using defaults" do
+			it("is a div") { expect(Node.new.to_s).to eq "<div></div>" }
+			it("starts at level 0") { expect(Node.new.level).to eq 0 }
+			it("has no children") { expect(Node.new.children).to eq [] }
+		end
+	end
 
-	it("will print its attributes") { has_attributes.intro.should eq "<div a='1' b='2'>" }
-	it("will print its id") { Node.from_hash(id: "asdf").intro.should eq "<div id='asdf'>" }
-	it("will print it class") { Node.from_hash(class: "asdf").intro.should eq "<div class='asdf'>" }
+	it("has a level") { expect(Node.new(level: 10).level).to eq 10 }
+	it("can level up") { expect(Node.new.level_up.level).to eq 1 }
 
-	it("has content") { Node.from_hash(content: "hello").content.should eq "hello" }
-	it("has blank content by default") { Node.new.content.should eq "" }
-
-	it("has an opening tag") { Node.new(tag: "p").intro.should eq "<p>" }
-	it("has a closing tag") { Node.new(tag: "p").outro.should eq "</p>" }
-
-	it("can become a string") { Node.new(tag: "p").to_s.should eq "<p></p>" }
-	
-	it("has children") { single_mom.children.size.should eq 1 }
-	it("will tell its children to become strings") { single_mom.to_s.should eq "<div><div></div></div>" }
+	context "when considering children" do
+		subject { parent = Node.new; parent << Node.new; parent }
+		it("can add children") { expect(subject.children.size).to eq 1 }
+		it("will show its children") { expect(subject.to_s).to eq "<div><div></div></div>" }
+	end
 end
