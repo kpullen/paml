2707cfe7f291a3d4cf9ff5021680a9e0bb821886
Kenneth Pullen <kenneth.pullen@gmail.com>
2013-11-06 21:53:03 -0800

    Document the Lonely node

--- lib/paml/node/lonely.rb
+++ lib/paml/node/lonely.rb
@@ -1,5 +1,7 @@
 module Paml
 	class Node
+		# I am a node that can have no children.
+		# I am bitter about it, so I'll raise an error if you try to give me children.
 		class Lonely < Node
 			def << _
 				raise "Nope"
