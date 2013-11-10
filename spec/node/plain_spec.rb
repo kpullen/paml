62e324c622ad688ca46114130a4c5f35786f95f6
Kenneth Pullen <kenneth.pullen@gmail.com>
2013-11-09 19:54:52 -0800

    Remove some useless files

--- spec/node/plain_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe Node::Plain do
-	context "when using defaults" do
-		it("is a div") { expect(Node.new.to_s).to eq "<div></div>" }
-		it("starts at level 0") { expect(Node.new.level).to eq 0 }
-		it("has no children") { expect(Node.new.children).to eq [] }
-	end
-end
