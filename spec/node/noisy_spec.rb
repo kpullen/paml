3c770e6f41f7a235c3ab315daf2f5b8f99434dfc
Kenneth Pullen <kenneth.pullen@gmail.com>
2013-11-09 23:34:09 -0800

    Document a test

--- spec/node/noisy_spec.rb
+++ spec/node/noisy_spec.rb
@@ -2,5 +2,5 @@ require "spec_helper"
 
 describe Node::Noisy do
 	subject { Node::Noisy.new(Line.new(content: "$thing")).to_s }
-	it { should eq "<?php echo $thing; ?>" }
+	it("echos the content") { should eq "<?php echo $thing; ?>" }
 end
